d5115808519635b01f24e784008d0a40c7b0ae61
rakibemon <rakibemon03@gmail.com>
2021-10-17 03:24:28 +0600

    fake data edit

--- src/hooks/useMeal.js
+++ src/hooks/useMeal.js
@@ -15,7 +15,7 @@ const useBreakfast = () => {
     }, []);
     const [dinners, setDinners] = useState([]);
     useEffect(() => {
-        fetch('https://raw.githubusercontent.com/rakibemon/hot-onion-restaurent-react/main/public/dinnerData.json')
+        fetch('https://raw.githubusercontent.com/rakibemon/hot-onion-restaurent-react/main/public/dinnersData.json')
             .then(response => response.json())
             .then(data => setDinners(data))
     }, []);
